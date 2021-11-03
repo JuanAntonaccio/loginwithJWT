960f0367a8ea67d1ba89d8fe40f4fec436e56764
JuanAntonaccio <jmantonaccio@gmail.com>
2021-11-03 21:05:28 +0000

    Agragamos algo y me da error la creacion de las bases de datos

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,7 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
@@ -32,6 +32,37 @@ def login():
 
 # Protect a route with jwt_required, which will kick out requests
 # without a valid JWT present.
+
+@api.route('/users', methods=['GET'])
+@jwt_required()
+def get_all_users():
+
+    usuarios=User.query.all()
+    usuarios = list(map(lambda user: user.serialize(), usuarios ))
+    if not usuarios:
+        return jsonify("no se encontraron usuarios"),404
+        
+    return jsonify(usuarios), 200
+
+@api.route('/users', methods=['POST'])
+def add_user():
+    # primero leo lo que viene en el body
+    body_us=json.loads(request.data)
+    #print (body_us)
+    usuario=User(email=body_us['email'],password=body_us['password'],is_active=body_us['is_active'])
+    db.session.add(usuario)               
+    db.session.commit()
+    
+    
+
+    usuarios=User.query.all()
+    usuarios = list(map(lambda usuario: usuario.serialize(), usuarios ))
+    if not usuarios:
+        return jsonify("no se encontraron usuarios"),404
+        
+    return jsonify(usuarios), 200
+
+
 @api.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
